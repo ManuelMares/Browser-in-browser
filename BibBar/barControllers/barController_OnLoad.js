2d468a2f3219baf665a891a3ea7af8ce9f4543f5
ManuelMares <fmms@outlook.com>
2024-07-25 20:47:15 -0700

    add settings file

--- BibBar/barControllers/barController_OnLoad.js
+++ BibBar/barControllers/barController_OnLoad.js
@@ -17,13 +17,7 @@ let _THIS_TAB_ID                                = null;
 let _THIS_BAR                                   = null;
 let _THIS_URL                                   = null;
 let _USE_TIMER                                  = false;
-const _CLOSE_TAB_TIME_IN_SECONDS_               = 15;                   //If you want the tabs to close after certain time, just indicate the time in seconds here
-const _NO_PIN_HOSTS                             = [ ]                   //Might nor be fully working yet
-const _URLS_NO_CLOSE_BUTTON_                    = [ ]                   //Add urls to this list to avoid users closing tabs by accident
-const _TABS_NO_CLOSE_AFTER_TIME_                = [ ];                  //take the 'close' button away from the user in this list of pages
 let _IS_MASTER                                  = false
-const _MAKE_PINNED_TABS_INTO_MASTER_TABS        = true                  //use for experiments. If set to true, the return, back and refresh buttons disappear
-
 let BIB_EXTENSION_ID                            = chrome.runtime.id;
 
 /**
---
+++ settings.js
@@ -0,0 +1,8 @@
+/*
+TODO: ADD A REAL DESCRIPTION FOR THIS FILE
+*/
+const _CLOSE_TAB_TIME_IN_SECONDS_               = 15;                   //If you want the tabs to close after certain time, just indicate the time in seconds here
+const _NO_PIN_HOSTS                             = [ ]                   //Might nor be fully working yet
+const _URLS_NO_CLOSE_BUTTON_                    = [ ]                   //Add urls to this list to avoid users closing tabs by accident
+const _TABS_NO_CLOSE_AFTER_TIME_                = [ ];                  //take the 'close' button away from the user in this list of pages
+const _MAKE_PINNED_TABS_INTO_MASTER_TABS        = true                  //use for experiments. If set to true, the return, back and refresh buttons disappear
